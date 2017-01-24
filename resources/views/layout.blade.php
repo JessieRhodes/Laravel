232bb38b79edc3b7253276e7360852d378336a09
JessieRhodes <jess.rhodes.87@gmail.com>
2017-01-24 14:29:25 -0600

    updated files

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!doctype>
+<html>
+ <head>
+   <title>My Site - @yield('title')</title>
+
+   <link rel="stylesheet" href="">
+ </head>
+ <body>
+
+   <section class="contents">
+     @yield('content')
+   </section>
+
+   <footer>
+     &copy; Copyright 2016
+   </footer>
+ </body>
+</html>
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('title')
+All Users
+@stop
+
+@section('content')
+<h1>All Users</h1>
+<ul class="list-group">
+@foreach ($users as $user)
+<li class="list-group-item">
+	<a href="/users/{{ $user->id }}">
+		{{ $user->name }}
+	</a>
+</li>
+@endforeach
+</ul>
+@stop
